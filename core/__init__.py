bcdb2a5312879558430927e229ef8e06faf58971
Mahima Gupta <mahima.gupta@target.com>
2025-08-16 14:28:48 +0530

    4

--- core/__init__.py
+++ core/__init__.py
@@ -5,17 +5,4 @@ Contains task orchestration, vector store components, and task configuration.
 
 from .task_orchestrator import TaskOrchestrator
 from .vectorstore import VectorStoreBuilder
-from .task_config import (
-    get_available_tasks,
-    get_task_parameters,
-    get_ideal_value,
-    get_ideal_value_reason,
-    get_task_param_blocks,
-    get_task_description,
-    get_task_icon,
-    get_task_ui_blocks,
-    ENCODER_ONLY_MODELS,
-    DECODER_ONLY_MODELS,
-    ENCODER_DECODER_MODELS
-)
 from utils.document_utils import create_documents_from_chunks, get_text_from_file 
